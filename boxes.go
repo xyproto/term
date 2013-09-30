fd81dc6556dad8d74ccb3fb51f51bbd63f6f54ae
Alexander Rødseth <rodseth@gmail.com>
2013-09-30 15:50:41 +0200

    Exported additional fields

--- boxes.go
+++ boxes.go
@@ -33,53 +33,53 @@ func NewBox() *Box {
 	return &Box{&Rect{0, 0, 0, 0}, &Rect{0, 0, 0, 0}}
 }
 
-func (r *Box) center(container *Box) {
+func (r *Box) Center(container *Box) {
 	widthleftover := container.inner.W - r.frame.W
 	heightleftover := container.inner.H - r.frame.H
 	r.frame.X = container.inner.X + widthleftover/2
 	r.frame.Y = container.inner.Y + heightleftover/2
 }
 
-func (r *Box) fill(container *Box) {
+func (r *Box) Fill(container *Box) {
 	r.frame.X = container.inner.X
 	r.frame.Y = container.inner.Y
 	r.frame.W = container.inner.W
 	r.frame.H = container.inner.H
 }
 
-func (r *Box) fillWithMargins(container *Box, margins int) {
-	r.fill(container)
+func (r *Box) FillWithMargins(container *Box, margins int) {
+	r.Fill(container)
 	r.frame.X += margins
 	r.frame.Y += margins
 	r.frame.W -= margins * 2
 	r.frame.H -= margins * 2
 }
 
-func (r *Box) fillWithPercentageMargins(container *Box, horizmarginp float32, vertmarginp float32) {
+func (r *Box) FillWithPercentageMargins(container *Box, horizmarginp float32, vertmarginp float32) {
 	horizmargin := int(float32(container.inner.W) * horizmarginp)
 	vertmargin := int(float32(container.inner.H) * vertmarginp)
-	r.fill(container)
+	r.Fill(container)
 	r.frame.X += horizmargin
 	r.frame.Y += vertmargin
 	r.frame.W -= horizmargin * 2
 	r.frame.H -= vertmargin * 2
 }
 
-func (r *Box) getContentPos() (int, int) {
+func (r *Box) GetContentPos() (int, int) {
 	return r.inner.X, r.inner.Y
 }
 
-func (r *Box) thirdSize(container *Box) {
+func (r *Box) SetThirdSize(container *Box) {
 	r.frame.W = container.inner.W / 3
 	r.frame.H = container.inner.H / 3
 }
 
-func (r *Box) thirdPlace(container *Box) {
+func (r *Box) SetThirdPlace(container *Box) {
 	r.frame.X = container.inner.X + container.inner.W/3
 	r.frame.Y = container.inner.Y + container.inner.H/3
 }
 
-func (r *Box) nicePlacement(container *Box) {
+func (r *Box) SetNicePlacement(container *Box) {
 	r.frame.X = container.inner.X
 	r.frame.Y = container.inner.Y
 	leftoverwidth := container.inner.W - r.frame.W
@@ -92,7 +92,7 @@ func (r *Box) nicePlacement(container *Box) {
 	}
 }
 
-func (b *Box) place(container *Box) {
+func (b *Box) Place(container *Box) {
 	b.frame.X = container.inner.X
 	b.frame.Y = container.inner.Y
 }
